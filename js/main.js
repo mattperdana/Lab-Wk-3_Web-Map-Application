080058fb514c321d252aa81725ff5bf919a47c64
Matthew Perdana Wicaksono <76998750+mattperdana@users.noreply.github.com>
2022-04-29 08:53:36 -0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,99 @@
+mapboxgl.accessToken =
+'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
+let map = new mapboxgl.Map({
+container: 'map', // container ID
+style: 'mapbox://styles/mapbox/outdoors-v11',
+projection: 'albers',
+zoom: 4, // starting zoom
+center: [-97, 37] // starting center
+});
+
+const counts = [5000, 10000, 20000],
+colors = ['rgb(208,209,230)', 'rgb(103,169,207)', 'rgb(1,108,89)'],
+radii = [5, 15, 20];
+
+//load data to the map as new layers.
+//map.on('load', function loadingData() {
+map.on('load', () => { //simplifying the function statement: arrow with brackets to define a function
+
+// when loading a geojson, there are two steps
+// add a source of the data and then add the layer out of the source
+map.addSource('counts', {
+    type: 'geojson',
+    data: 'assets/counts.geojson'
+});
+
+map.addLayer({
+        'id': 'counts',
+        'type': 'circle',
+        'source': 'counts',
+        //'minzoom': 5,
+        'paint': {
+            // increase the radii of the circle as the zoom level and dbh value increases
+            'circle-radius': {
+                'property': 'cases',
+                'stops': [
+                    [{
+                        zoom: 5,
+                        value: counts[0]
+                    }, radii[0]],
+                    [{
+                        zoom: 5,
+                        value: counts[1]
+                    }, radii[1]],
+                    [{
+                        zoom: 5,
+                        value: counts[2]
+                    }, radii[2]]
+                ]
+            },
+            'circle-color': {
+                'property': 'cases',
+                'stops': [
+                    [counts[0], colors[0]],
+                    [counts[1], colors[1]],
+                    [counts[2], colors[2]]
+                ]
+            },
+            'circle-stroke-color': 'white',
+            'circle-stroke-width': 1,
+            'circle-opacity': 0.6
+        }
+    },
+    'waterway-label'
+);
+
+
+// click on tree to view magnitude in a popup
+map.on('click', 'counts', (event) => {
+    new mapboxgl.Popup()
+        .setLngLat(event.features[0].geometry.coordinates)
+        .setHTML(`<strong>Counts:</strong> ${event.features[0].properties.cases}<br>${event.features[0].properties.county}`)
+        .addTo(map);
+});
+
+});
+
+
+// create legend
+const legend = document.getElementById('legend');
+
+//set up legend grades and labels
+var labels = ['<strong>Counts Numbers</strong>'], vbreak;
+//iterate through grades and create a scaled circle and label for each
+for (var i = 0; i < counts.length; i++) {
+vbreak = counts[i];
+// you need to manually adjust the radius of each dot on the legend 
+// in order to make sure the legend can be properly referred to the dot on the map.
+dot_radii = 2 * radii[i];
+labels.push(
+    '<p class="break"><i class="dot" style="background:' + colors[i] + '; width: ' + dot_radii +
+    'px; height: ' +
+    dot_radii + 'px; "></i> <span class="dot-label" style="top: ' + dot_radii / 2 + 'px;">' + vbreak +
+    '</span></p>');
+
+}
+const source =
+'<p style="text-align: right; font-size:10pt">Source: <a href="https://github.com/nytimes/covid-19-data/blob/43d32dde2f87bd4dafbb7d23f5d9e878124018b8/live/us-counties.csv">The New York Times</a></p>';
+
+legend.innerHTML = labels.join('') + source;
